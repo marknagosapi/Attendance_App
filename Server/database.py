fafb06b829c446bb20d1a39ff1d62d4daa2e100c
lajos <lajos.zalan@student.ms.sapientia.ro>
2023-10-30 18:58:29 +0200

    Login

--- Server/database.py
+++ Server/database.py
@@ -1,18 +1,49 @@
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate("db_key.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-ref_faces = db.collection("Faces")
+ref_users = db.collection("Users")
 
-def uploadFace(userName,encodedFace:list):
-    data = {}
-    data["encodedFace"] = encodedFace
-    if userName is not None: data["userName"] = userName
-    ref_faces.add(data)
+def uploadFace(userId,encodedFace:list):
+    data = {
+        "encodedFace": encodedFace
+    }
+    ref_users.document(userId).update(data)
 
-def getStudents():
-    return ref_faces.stream()
+def getStudentsFacesAndIds():
+    users = ref_users.stream()
+    faces = []
+    ids = []
+    for user in users:
+        ids.append(user.id)
+        faces.append(user.to_dict()["encodedFace"])
+    return {
+        "ids": ids,
+        "faces": faces
+    }
+
+def getStudentById(id: str):
+    return ref_users.document(id).get().to_dict()
+
+def uploadNewUser(user):
+    isUserExists = len(ref_users.where("email", "==", user.email).where("userName", "==", user.userName).get()) != 0
+
+    if isUserExists: return False
+
+    userData = {
+        "userName": user.userName,
+        "password": user.password,
+        "email": user.email,
+        "userType": user.userType
+    }
+    ref_users.add(userData)
+
+    return True
+
+def getUserByNameAndPassword(userName, password):
+    user = ref_users.where("userName", "==", userName).where("password", "==", password).get()
+    if user == []: return None
+    return user[0].id
